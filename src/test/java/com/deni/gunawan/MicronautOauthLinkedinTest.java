d3f7e7d1fbc3ed9e87be069420d231ef1834d749
Deni Gunawan <artdengun>
2022-08-27 18:17:59 +0000

    Initial commit

---
+++ src/test/java/com/deni/gunawan/MicronautOauthLinkedinTest.java
@@ -0,0 +1,21 @@
+package com.deni.gunawan;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MicronautOauthLinkedinTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
